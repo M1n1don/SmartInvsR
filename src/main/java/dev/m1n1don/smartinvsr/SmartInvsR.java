22712b1483dd056ad8591bb9544324d8419f11d1
みにどん <48941476+M1n1don@users.noreply.github.com>
2022-06-23 20:38:51 +0900

    Init

---
+++ src/main/java/dev/m1n1don/smartinvsr/SmartInvsR.java
@@ -0,0 +1,17 @@
+package dev.m1n1don.smartinvsr;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SmartInvsR extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
